6eb11bc2b25f2add1df76177b5da3c3e0ed9f910
obwogo <tobwogo@gmail.com>
2023-02-16 00:15:06 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *main- C program that prints exactly with proper grammar, but the outcome is a piece of art
+ *main- C program that prints exactly with proper grammar,
  *Return: 0 (success)
  */
 int main(void)
