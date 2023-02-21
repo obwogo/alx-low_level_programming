174a02ae24267a9655a56fe24d698066d473161e
obwogo <tobwogo@gmail.com>
2023-02-21 05:42:30 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,8 +5,9 @@
  */
 void print_alphabet(void)
 {
-char la;
-for (la = 'a'; la <= 'z'; la++)
-_putchar(la);
-_putchar('\n');
+	char c = 'a'
+		while( c <= 'z')
+		{_putchar(c);
+		c++;
+		}_putchar('\n');
 }
