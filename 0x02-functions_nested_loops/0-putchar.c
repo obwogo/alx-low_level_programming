a993a88dc8c134104d68a6a42beaca2a5d4fe8fd
obwogo <tobwogo@gmail.com>
2023-02-21 04:02:56 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "stdio.h"
 
 /**
   * main - ...
@@ -9,15 +9,6 @@
   */
 int main(void)
 {
-		_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
-	return (0);
+printf("_putchar\n");
+return (0);
 }
