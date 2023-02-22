cf1b9475d02b8adf8e1645c305a9033305097c9e
obwogo <tobwogo@gmail.com>
2023-02-22 00:05:55 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,49 +10,46 @@
  */
 void print_times_table(int n)
 {
-	int num, mult, prod;
+int num, mult, prod;
 
-	if (n == 0)
-	{
-		_putchar('0');
-		_putchar('\n');
-	}
-	else if (n > 0 && n <= 15)
-	if (n >= 0 && n <= 15)
-	{
-		for (num = 0; num <= n; num++)
-		{
-			_putchar('0');
-			_putchar(',');
-			_putchar(' ');
+if (n == 0)
+{
+_putchar('0');
+_putchar('\n');
+}
+else if (n > 0 && n <= 15)
+if (n >= 0 && n <= 15)
+{
+for (num = 0; num <= n; num++)
+{
+_putchar('0');
+_putchar(',');
+_putchar(' ');
 
-			for (mult = 1; mult <= n; mult++)
-			{
-				_putchar(',');
-				_putchar(' ');
+for (mult = 1; mult <= n; mult++)
+{
+prod = num * mult;
 
-				prod = num * mult;
-
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 9)
-					_putchar(' ');
-				if (prod >= 100)
-				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
-				}
-				else if (prod <= 99 && prod >= 10)
-				{
-					_putchar((prod / 10) + '0');
-				}
-				_putchar((prod % 10) + '0');
-				if (mult == n)
-					continue;
-				_putchar(',');
-				_putchar(' ');
-			}
-			_putchar('\n');
-		}
-	}
+if (prod <= 99)
+_putchar(' ');
+if (prod <= 9)
+_putchar(' ');
+if (prod >= 100)
+{
+_putchar((prod / 100) + '0');
+_putchar(((prod / 10)) % 10 + '0');
+}
+else if (prod <= 99 && prod >= 10)
+{
+_putchar((prod / 10) + '0');
+}
+_putchar((prod % 10) + '0');
+if (mult == n)
+continue;
+_putchar(',');
+_putchar(' ');
+}
+_putchar('\n');
+}
+}
 }
