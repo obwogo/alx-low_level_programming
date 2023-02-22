04bef955342fbb41ac211f18844543d224ae95ee
obwogo <tobwogo@gmail.com>
2023-02-21 23:41:50 -0800

    /100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -36,7 +36,14 @@ void print_times_table(int n)
 				if (prod <= 99)
 					_putchar(' ');
 				if (prod <= 9)
-@@ -44,10 +42,6 @@ void print_times_table(int n)
+					_putchar(' ');
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
 					_putchar((prod / 10) + '0');
 				}
 				_putchar((prod % 10) + '0');
@@ -47,3 +54,5 @@ void print_times_table(int n)
 			}
 			_putchar('\n');
 		}
+	}
+}
