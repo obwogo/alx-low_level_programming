2e338163ce6870f2ec02794f30e1b7f7b42f5d1a
obwogo <tobwogo@gmail.com>
2023-02-24 14:32:25 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,18 +4,23 @@
  * print_number - Function that prints an integer.
  * @n: int type number
  * Description: Can only use _putchar to print.
+ * print_number - prints an integer
+ * @n: integer to be printed
  */
 void print_number(int n)
 {
 	long m; /* power of 10 */
 	int c; /* boolean check */
 	long num; /* convert int to long */
+	unsigned int n1;
 
 	num = n;
 	/* negatives */
 	if (num < 0)
+	if (n < 0)
 	{
 		num *= -1;
+		n1 = -n;
 		_putchar('-');
 	}
 
@@ -23,15 +28,18 @@ void print_number(int n)
 	m = 1;
 	c = 1;
 	while (c)
+	} else
 	{
 		if (num / (m * 10) > 0)
 			m *= 10;
 		else
 			c = 0;
+		n1 = n;
 	}
 
 	/* count down */
 	while (num >= 0)
+	if (n1 / 10)
 	{
 		if (m == 1)
 		{
@@ -43,5 +51,8 @@ void print_number(int n)
 			_putchar((num / m % 10) + '0');
 			m /= 10;
 		}
+		print_number(n1 / 10);
 	}
+
+	_putchar((n1 % 10) + '0');
 }
