3f3ba2e2e46ed0ad0c05062f0d4f6e79e6d33bd1
obwogo <tobwogo@gmail.com>
2023-03-12 09:47:01 -0700

    program that prints its name, followed by a new line.

--- 0-whatsmyname.c
+++ 0-whatsmyname.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-#include "main.h"
 /**
- * main - prints its name + \n
- * @argc: number of arguments typed
- * @argv: array pointing to arguements
- * Return: 0
+ *main-prints the program name with a new line
+ *@argc:The number of arguments supplied to the program
+ *@argv:an array of pointers to the argument
+ *
+ *Return: Always 0
  */
-int main(int argc, char *argv[])
-{
-	argc = 0;
 
-	printf("%s\n", argv[argc]);
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
 	return (0);
 }
