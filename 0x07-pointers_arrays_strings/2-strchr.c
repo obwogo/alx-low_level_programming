7c9f971fe47bd973089abffadaf7c2c7a96dbfb9
obwogo <tobwogo@gmail.com>
2023-03-12 09:33:18 -0700

    locally tested all, inital commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -18,5 +18,6 @@ char *_strchr(char *s, char c)
 			return (s + i);
 		i++;
 	}
+
 	return (NULL);
 }
--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * _puts_recursion - function like puts();
- * @s: input
- * Return: Always 0 (Success)
+ *_puts_recursion - prints a string, followed by a new line;
+ *
+ * Return: Always 0.
  */
-void _puts_recursion(char *s)
+
+void _puts_recursion(char *s);
 {
-	if (*s == '\0')
-		_putchar('\n');
-	else
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
+if(*s=='\n')
+	_putchar('\n');
+
 }
+
